fe005c0cccdb40dd61d3c7210efd44eaafe9d211
Salmo Alves <salmo7848@outlook.com>
2022-02-10 20:39:27 -0300

    Criada uma exceção: classe BoardException, e programação defensiva

--- src/boardGame/Board.java
+++ src/boardGame/Board.java
@@ -9,6 +9,9 @@ public class Board {
 	
 	
 	public Board(int row, int columns) {
+		if(row < 1 || columns < 1) {
+			throw new BoardException("Erro a criar tabuleiro, numero de linhas e colunas menor que 1");
+		}
 		this.row = row;
 		this.columns = columns;
 		piece = new Peace[row][columns];
@@ -17,26 +20,46 @@ public class Board {
 	public int getRow() {
 		return row;
 	}
-	public void setRow(int row) {
-		this.row = row;
-	}
 	public int getColumns() {
 		return columns;
 	}
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
 		
 	public Peace piece(int row, int column) {
+		if(!positionExists(row,column)) {
+			throw new BoardException("Não exite a posição!");
+		}
 		return piece[row][column];
 	}
 
 	public Peace piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Não exite a posição!");
+		}
 		return piece[position.getRow()][position.getColumns()];
 	}
 	
 	public void pleacePiece(Peace piec, Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("Já temos uma peça na posição ("+position+")!");
+		}
 		piece[position.getRow()][position.getColumns()] = piec;
 		piec.position = position;
 	}
+	
+	private boolean positionExists(int rows, int column) {
+		return rows >=0 && rows < row && column >= 0 && column < columns;
+		
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumns());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Não exite a posição!");
+		}
+		return piece(position) != null;
+	}
+	
 }
